54a55e6006bbdb189669805c49b5b242fc414a6d
Zipofar Ago <admin@zipofar.ru>
2018-12-11 18:41:51 +0300

    Add seo page

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('pagespeed');
 });
 
 router.get('/pagespeed', function(req, res, next) {
   res.render('pagespeed');
+});
+
+router.get('/pagespeed-result', function(req, res, next) {
+  res.render('pagespeed-result');
+});
+
+router.get('/seo', function(req, res, next) {
+  res.render('seo');
+});
+
+router.get('/seo-result', function(req, res, next) {
+  res.render('seo-result');
 });
 
 module.exports = router;
